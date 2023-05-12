f03ba0ca2d80e2a09e8b5c665ab0890cb6654056
Ritesh <ritesh88944@gmail.com>
2023-05-12 14:14:52 +0530

    typo fixes

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -9,7 +9,7 @@ const Geography = () => {
     const colors = tokens(theme.palette.mode)
     return (
         <Box m='20px'>
-            <Header title='Bar Chart' subtitle='Simple  Geo map' />
+            <Header title='Geo Map' subtitle='Simple  Geo map' />
             <Box height='75vh' border={`1px solid ${colors.grey[100]}`}>
                 <GeographyChart />
             </Box>
--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -7,7 +7,7 @@ import LineChart from "../../components/LineChart"
 const Line = () => {
     return (
         <Box m='20px'>
-            <Header title='Bar Chart' subtitle='Simple Line Chart' />
+            <Header title='Line Chart' subtitle='Simple Line Chart' />
             <Box height='75vh' >
                 <LineChart />
             </Box>
